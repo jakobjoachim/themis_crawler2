b94df947027484aec98d33f467df5e1ba3383c71
Jakob Joachim <jak.joachim@gmail.com>
2016-05-09 11:49:20 +0200

    fkjgd

--- modifyDatabase.py
+++ modifyDatabase.py
@@ -10,22 +10,19 @@ r.db("themis").table_create("crawledUrls").run(c)
 
 cursor = r.db("themis").table("pagesNew2").run(c)
 titles = set()
-
 for document in cursor:
     title = document['title']
     content = document['content']
     author = document['author']
     url = document['uri']
     date = document['date']
-
     if (title in titles):
+        print("breaked")
         break
     titles.add(title)
-
     date = datetime.strptime(date, "%Y%m%d")
     utc = timezone('UTC')
     date = utc.localize(date)
-
     data = {
        'title' : title,
        'content' : content,
@@ -33,11 +30,9 @@ for document in cursor:
        'url' : url,
        'date' : date
     }
-
     crawled = {
        'url' : url,
        'crawled' : 1
     }
-
-    r.db("themis").table("pages").insert(date).run(c)
+    r.db("themis").table("pages").insert(data).run(c)
     r.db("themis").table("crawledUrls").insert(crawled).run(c)
